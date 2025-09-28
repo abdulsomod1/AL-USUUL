fe77af19f9beae80e4f4e52f79da21234797b4bb
tenifayo abdulsomod <tenifayoabdulsomod@gmail.com>
2025-09-28 16:43:59 -0700

    Still writing some codes to make my web look imagined

--- form/script.js
+++ form/script.js
@@ -1,14 +1,26 @@
-var form = document.getElementById('usuul');
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  fetch(form.action, {
-    method: "POST",
-    body: new FormData(document.getElementById("usuul")),
-  }).then(
-    response => response.json()
-  ).then((html) => {
-    alert('submitted Thanks for the Application ' + document.getElementById('usuul-name').value)
-    window.location.href = '';
-    location.reload();
-  })
-})
+const form = document.getElementById('usuul');
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        var formData = new FormData(form);
+        var ex = document.getElementById("ex").checked;
+        var age = document.getElementById("age").checked;
+
+        if (age) {
+          formData.append("age", "Yes");
+        } else {
+          formData.append("age", "No");
+        }
+        if (ex) {
+          formData.append("ex", "Yes");
+        } else {
+          formData.append("ex", "No");
+        }
+
+        fetch(scriptURL, { method: "POST", body: formData })
+          .then((response) => {
+            swal("Done", "Submitted Successfully.", "success");
+          })
+          .catch((error) => {
+            swal("Error", "Something went wrong. please try again!", "error");
+          });
+      });
