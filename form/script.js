48eda9859dc0945ffabb1d71df8b020bc935c9e4
tenifayo abdulsomod <tenifayoabdulsomod@gmail.com>
2025-09-28 17:13:58 -0700

    Still writing code

--- form/script.js
+++ form/script.js
@@ -1,26 +1,14 @@
-const form = document.getElementById('usuul');
-      form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        var formData = new FormData(form);
-        var ex = document.getElementById("ex").checked;
-        var age = document.getElementById("age").checked;
-
-        if (age) {
-          formData.append("age", "Yes");
-        } else {
-          formData.append("age", "No");
-        }
-        if (ex) {
-          formData.append("ex", "Yes");
-        } else {
-          formData.append("ex", "No");
-        }
-
-        fetch(scriptURL, { method: "POST", body: formData })
-          .then((response) => {
-            swal("Done", "Submitted Successfully.", "success");
-          })
-          .catch((error) => {
-            swal("Error", "Something went wrong. please try again!", "error");
-          });
-      });
+var form = document.getElementById('usuul');
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(document.getElementById("usuul")),
+  }).then(
+    response => response.json()
+  ).then((html) => {
+    alert('submitted || Thank you' + document.getElementById('usuul-name').value)
+    window.location.href = '';
+    location.reload();
+  })
+})
